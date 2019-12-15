81b0a5d153aa8b3678c7d52aecd6b19d6bdabb73
anksaiki <asaikia@hawk.iit.edu>
2019-12-14 22:14:46 -0600

    url redirect code

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
-var url = window.location.href;
-
-url = new URL(url);
-var algo = url.searchParams.get("algo");
-if (algo == undefined || algo == "djikstra") {
-  $("#nav-algo").append("  " + "Djikstra's Algorithm");
-  $("#current-algo").attr("value", "shortest_path");
-} else if (algo == "dfs") {
-  $("#nav-algo").append("  " + "Depth First Search");
-  $("#current-algo").attr("value", "tree");
-}
-
-document.ready(function() {
+$(document).ready(function() {
   var url = window.location.href;
   if (url.indexOf("index.html") == -1) {
     url = url + "index.html?algo=djikstra";
-    window.location.href(url);
+    window.location.assign(url);
+  }
+  url = new URL(url);
+  var algo = url.searchParams.get("algo");
+  if (algo == undefined || algo == "djikstra") {
+    $("#nav-algo").append("  " + "Djikstra's Algorithm");
+    $("#current-algo").attr("value", "shortest_path");
+  } else if (algo == "dfs") {
+    $("#nav-algo").append("  " + "Depth First Search");
+    $("#current-algo").attr("value", "tree");
   }
 });
