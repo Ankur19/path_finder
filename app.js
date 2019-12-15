e69178a23bb9eea80091c84eebf85ea2e7ba8d6b
anksaiki <asaikia@hawk.iit.edu>
2019-12-14 21:57:39 -0600

    add url changes

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var url = window.location.href;
+
+if (url.indexOf("index.html") == -1) {
+  url = url + "index.html?algo=djikstra";
+  window.location.href = url;
+}
+
 url = new URL(url);
 var algo = url.searchParams.get("algo");
-
 if (algo == undefined || algo == "djikstra") {
   $("#nav-algo").append("  " + "Djikstra's Algorithm");
   $("#current-algo").attr("value", "shortest_path");
