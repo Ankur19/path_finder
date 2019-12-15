2e549d6ee0511abcbc8baad2bd587e82d18cf810
anksaiki <asaikia@hawk.iit.edu>
2019-12-14 22:07:49 -0600

    url changes trial and error

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 var url = window.location.href;
 
-if (url.indexOf("index.html") == -1) {
-  url = url + "index.html?algo=djikstra";
-  window.location.href(url);
-}
-
 url = new URL(url);
 var algo = url.searchParams.get("algo");
 if (algo == undefined || algo == "djikstra") {
@@ -14,3 +9,11 @@ if (algo == undefined || algo == "djikstra") {
   $("#nav-algo").append("  " + "Depth First Search");
   $("#current-algo").attr("value", "tree");
 }
+
+document.ready(function() {
+  var url = window.location.href;
+  if (url.indexOf("index.html") == -1) {
+    url = url + "index.html?algo=djikstra";
+    window.location.href(url);
+  }
+});
